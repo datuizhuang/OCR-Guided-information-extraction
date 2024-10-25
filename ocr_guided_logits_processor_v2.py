2d7c3cacec95757641efa12a33276360fd8fb413
datuizhuang <chenzhuang_mail@163.com>
2024-10-25 15:00:18 +0800

    add v3

--- ocr_guided_logits_processor_v2.py
+++ ocr_guided_logits_processor_v2.py
@@ -1,13 +1,12 @@
+import re
 from queue import Queue
 
 import torch
-# replace to BaseTokenzier
-# from transformers.models.qwen2.tokenization_qwen2_fast import Qwen2TokenizerFast
 from transformers import AutoTokenizer
 
 
 SPLIT_CHAR_LIST: list[str] = [':', '-']
-
+RE_PATTERN_LIST: list[str] = ['\D+|\d']
 
 def split_text_func(texts: list[str]) -> list[str]:
     """ split text
@@ -38,6 +37,13 @@ def split_text_func(texts: list[str]) -> list[str]:
                 new_texts.append(text[:idx + 1])
                 q.put(text[idx + 1: ])
         texts = list(set(new_texts))
+    
+    new_texts: list[str] = []
+    for pattern in RE_PATTERN_LIST:
+        for text in texts:
+            new_texts.extend(re.findall(pattern=pattern, string=text))
+    new_texts.extend(texts)
+    texts = list(set(texts))
     return texts
 
 
@@ -45,6 +51,7 @@ class TokenTree:
     def __init__(self, token: int = -1):
         self.childrens: dict[int, "TokenTree"] = dict()
         self.token = token
+        self.maybe_last = False
 
     def __contains__(self, token: int) -> bool:
         return token in self.childrens
@@ -71,6 +78,7 @@ class TokenTree:
                     cur_root = cur_root[token]
                 else:
                     cur_root = cur_root.add_node(token=token, node=TokenTree(token=token))
+            cur_root.maybe_last = True
             cur_root = root
         return root
 
@@ -81,15 +89,18 @@ class OCRLogitProcessor:
         ocr_tree: TokenTree,
         tokenizer: AutoTokenizer,
         special_tree: TokenTree = None,
+        special_tokens: list[int] = None,
         filter_value: int = -float("Inf"),
     ):
         # origin pointer
         self.ocr_tree = ocr_tree
-        self.cur_ocr_tree = ocr_tree
+        self.cur_ocr_trees = [ocr_tree]
         
         # current pointer
         self.special_tree = special_tree
-        self.cur_special_tree = special_tree
+        self.cur_special_trees = [special_tree]
+        
+        self.special_tokens = special_tokens or list()
         
         self.filter_value = filter_value
         
@@ -98,9 +109,9 @@ class OCRLogitProcessor:
     
     def clear(self):
         self.is_valid = True
-        self.cur_ocr_tree = self.ocr_tree
-        self.cur_special_tree = self.special_tree
-    
+        self.cur_ocr_trees = [self.ocr_tree]
+        self.cur_special_trees = [self.special_tree]
+
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
         """process logit by ocr informations
 
@@ -121,10 +132,14 @@ class OCRLogitProcessor:
         
         vocab_size: int = input_ids.size()[-1]
         dim: int = scores.dim()
-        
-        can_next_tokens: set[int] = set(self.cur_ocr_tree.childrens.keys())
-        can_next_tokens |= set(self.cur_special_tree.childrens.keys())
-        can_next_tokens = list(can_next_tokens)
+
+        can_next_tokens: set[int] = set()
+        for node in self.cur_ocr_trees:
+            can_next_tokens |= set(node.childrens.keys())
+        for node in self.cur_special_trees:
+            can_next_tokens |= set(node.childrens.keys())
+        can_next_tokens |= self.special_tokens
+        can_next_tokens: list[int] = list(can_next_tokens)
         
         mask = torch.BoolTensor = torch.ones((vocab_size), device=input_ids.device, dtype=torch.bool)
         mask[can_next_tokens] = 0
@@ -143,31 +158,53 @@ class OCRUpdateCriteria:
         assert bs == 1, 'now only support bs == 1'
         next_token = input_ids[..., -1]
         next_token: int = int(next_token[0])
-
-        if next_token in self.processor.cur_ocr_tree:
-            self.processor.cur_ocr_tree = self.processor.cur_ocr_tree[next_token]
-            if self.processor.cur_ocr_tree.is_leaf:
-                self.processor.cur_ocr_tree = self.processor.ocr_tree
-        elif next_token in self.processor.cur_special_tree:
-            self.processor.cur_special_tree = self.processor.cur_special_tree[next_token]
-            if self.processor.cur_special_tree.is_leaf:
-                self.processor.cur_special_tree = self.processor.special_tree
-        else:
+        
+        in_ocr, in_special = False, False
+        
+        cur_ocr_trees: set[TokenTree] = set()
+        for node in self.processor.cur_ocr_trees:
+            if next_token in node:
+                in_ocr = True
+                tmp_node = node[next_token]
+                cur_ocr_trees.add(self.processor.ocr_tree if tmp_node.is_leaf else tmp_node)
+                if tmp_node.maybe_last:
+                    cur_ocr_trees.add(self.processor.ocr_tree)
+        self.processor.cur_ocr_trees = cur_ocr_trees
+        
+        cur_special_trees: set[TokenTree] = set()
+        for node in self.processor.cur_special_trees:
+            if next_token in node:
+                in_special = True
+                tmp_node = node[next_token]
+                cur_special_trees.add(self.processor.special_tree if tmp_node.is_leaf else tmp_node)
+                if tmp_node.maybe_last:
+                    cur_special_trees.add(self.processor.special_tree)
+        self.processor.cur_special_trees = cur_special_trees
+        
+        if not in_ocr and not in_special and next_token not in self.processor.special_tokens:
             self.processor.is_valid = False
 
         return torch.full((bs, ), False, device=input_ids.device, dtype=torch.bool)
 
 
 def get_ocr_logit_processor(
-    texts: list[str], tokenizer: AutoTokenizer, specials: list[str] = [], split_text: bool = True
+    texts: list[str], 
+    tokenizer: AutoTokenizer, 
+    specials: list[str] = [], 
+    special_chars: list[str] = [],
+    split_text: bool = True,
 ) -> OCRLogitProcessor:
     if split_text:
         texts: list[str] = split_text_func(texts=texts)
 
     ocr_tree: TokenTree = TokenTree.build_from_ocr(texts=texts, tokenizer=tokenizer)
     special_tree: TokenTree = TokenTree.build_from_ocr(texts=specials, tokenizer=tokenizer)
+    special_tokens: list[int] = []
+    for char in special_chars:
+        special_tokens.extend(tokenizer.encode(char))
+    special_tokens = list(set(special_tokens))
     ocr_logit_processor = OCRLogitProcessor(
-        ocr_tree=ocr_tree, tokenizer=tokenizer, special_tree=special_tree
+        ocr_tree=ocr_tree, tokenizer=tokenizer, special_tree=special_tree, special_tokens=special_tokens
     )
     return ocr_logit_processor
 
@@ -176,6 +213,7 @@ def get_processor_and_criteria(
     texts: list[str],
     tokenizer: AutoTokenizer,
     specials: list[str],
+    special_chars: list[str] = [],
     split_text: bool = True
 ):
     """
@@ -197,7 +235,7 @@ def get_processor_and_criteria(
         _description_
     """
     logit_processor = get_ocr_logit_processor(
-        texts=texts, tokenizer=tokenizer, specials=specials, split_text=split_text
+        texts=texts, tokenizer=tokenizer, specials=specials, split_text=split_text, special_chars=special_chars
     )
     criteria = OCRUpdateCriteria(logit_processor=logit_processor)
     return logit_processor, criteria
